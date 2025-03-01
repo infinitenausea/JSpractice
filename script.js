ed8141ad0d584aa0578f88c19e3371a5e00b774e
Anatolii Orlov <anatoly.orlov93@gmail.com>
2025-03-01 04:16:26 +0400

    added project details

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Проект";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 10000;
+let rollback = 20;
+let fullPrice = 20000;
+let adaptive = true;
 
-alert("This is random text!");
-console.log("This is random message!")
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов: " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта: " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log("Процент отката посреднику за работу: " + (fullPrice*(rollback/100)) + " рублей");
